b4b6c36b3f136ed26235a25329f0e87991b34c1b
James McCay <jwmccay@pm.me>
2024-06-15 10:05:35 -0700

    Ready for next book

--- source/conf.py
+++ source/conf.py
@@ -14,11 +14,15 @@ release = '0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+}
 
 
 # -- Options for HTML output -------------------------------------------------
